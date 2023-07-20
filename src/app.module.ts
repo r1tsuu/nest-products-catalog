3afa2843efec301dfa7a0900e4b7e8dee3e69273
r1tsuu <enjoythepain1337@gmail.com>
2023-07-20 23:39:43 +0300

    initialize cart module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
   imports: [
@@ -24,6 +25,7 @@ import { OrdersModule } from './orders/orders.module';
     AuthModule,
     ProductsModule,
     OrdersModule,
+    CartModule,
   ],
 })
 export class AppModule {}
